33dcea2d651074996c1dead7359808cb42beeaa4
ABID <khalil@tanitweb.com>
2023-06-15 14:56:20 +0100

    [feature-8580] - Change naming of function

--- src/Button.js
+++ src/Button.js
@@ -1,20 +1,17 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 export default function Button() {
-    const [nbclick,setNbClick] = useState(0);
-    
-    function click(){
-        setNbClick( nbclick + 1);
-    }
-    
-    return(
-        <>
-          <button onClick={click}>
-             Click Here
-          </button>
-          <br/>
-          <b>Clicked {nbclick} times </b>
-        </>
-        
-    );
+  const [nbclick, setClickNumber] = useState(0);
+
+  function click() {
+    setClickNumber(nbclick + 1);
+  }
+
+  return (
+    <>
+      <button onClick={click}>Click Here</button>
+      <br />
+      <b>Clicked {nbclick} times </b>
+    </>
+  );
 }
